b9478e0e1924b06e4a8e370ad480c499ab5466c2
Loren Gordon <lorengordon@users.noreply.github.com>
2020-11-24 07:53:12 -0800

    Updates test to create the detector in the master account

--- tests/create_guardduty_member/main.tf
+++ tests/create_guardduty_member/main.tf
@@ -9,14 +9,29 @@ provider aws {
   profile = "resource-owner"
 }
 
-module "create_guardduty_member" {
+module create_guardduty_member {
   source = "../../"
 
   providers = {
-    aws        = "aws"
-    aws.master = "aws.resource-owner"
+    aws        = aws
+    aws.master = aws.resource-owner
   }
 
-  guardduty_master_detector_id = "<insert_detector_id>"
-  email_address                = "<insert_email>"
+  guardduty_master_detector_id = aws_guardduty_detector.master.id
+  email_address                = var.member_email
+
+  depends_on = [
+    aws_guardduty_detector.master
+  ]
+}
+
+resource aws_guardduty_detector master {
+  provider = aws.resource-owner
+
+  enable = true
+}
+
+variable member_email {
+  description = "Email address associated with the member account. Required input for the Guardduty member invitation."
+  type        = string
 }
