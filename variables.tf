4e266d99d8bc6784fd5caa7be672b1b235fdf715
Loren Gordon <lorengordon@users.noreply.github.com>
2020-11-24 07:41:31 -0800

    Makes required variables actually required

--- variables.tf
+++ variables.tf
@@ -1,17 +1,9 @@
-variable "guardduty_master_detector_id" {
-  description = "GuardDuty Detector ID for master account"
-  type        = string
-  default     = null
-}
-
-variable "tags" {
-  description = "A map of tags to add to the CFN resource for GuardDuty"
-  type        = map(string)
-  default     = {}
-}
-
 variable "email_address" {
   description = "Email address associated with the member account. Required input for the GuardDuty member invitation."
   type        = string
-  default     = null
+}
+
+variable "guardduty_master_detector_id" {
+  description = "GuardDuty Detector ID for master account"
+  type        = string
 }
